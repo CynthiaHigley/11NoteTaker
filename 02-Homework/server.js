877290c28915d47f955b7e2b51b12a9b0839e10e
Cynthia Higley <calgonquin12@yahoo.com>
2020-02-15 23:03:28 -0700

    process

---
+++ 02-Homework/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/frontend"));
+
+require("./backend/routes/apiRoutes")(app);
+require("./backend/routes/htmlRoutes")(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
